1876502e0526d263f39cbe4dc92efe4881ebe9dd
fransi-kurti <fransi.kurti@gmail.com>
2023-02-19 22:51:25 +0100

    finished javascript & added some transition effect

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const btnEl = document.getElementById("btn");
 
+const videoEl = document.querySelector(".video-popup-container");
+
+const closeIconEl = document.getElementById("close_icon");
+
+const video = document.getElementById("video");
+
 btnEl.addEventListener("click", ()=>{
-    
-})
+    videoEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", ()=>{
+    videoEl.classList.add("active");
+    video.pause();
+    video.currentTime = 0;
+});
